603d0dfc9440858fd80752ff807776253de7e61d
Eric Schulte <schulte.eric@gmail.com>
2021-08-30 21:26:49 -0400

    Simplify eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,3 @@
-const csv = require("csv-parse")
-var Readable = require('stream').Readable
-const yaml = require("js-yaml");
 const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
@@ -10,34 +7,14 @@ module.exports = function(eleventyConfig) {
   md.use(require("markdown-it-deflist"));
   md.use(require("markdown-it-anchor"));
   eleventyConfig.setLibrary("md", md);
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
+    return md.render(content);
+  });
 
   // clean-css
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
-
-  eleventyConfig.addPairedShortcode("markdown", (content) => {
-    return md.render(content);
-  });
-
-  // Parse CSV
-  eleventyConfig.addDataExtension("csv", (contents) => {
-    var s = new Readable()
-    s.push(contents)
-    s.push(null)
-    results = [];
-    s.pipe(csv()).on('data', (data) => results.push(data));
-    return results;
-  });
-
-  // Parse YAML
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-
-  // Files to copy
-  // eleventyConfig.addPassthroughCopy("favicon.ico");
-
-  // Paths
-  // eleventyConfig.addPassthroughCopy("img");
 
   return eleventyConfig;
 };
